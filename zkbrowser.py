0752f7c014e9f7a37f826d374cae387d16821bd2
prixeus <tamas.brenner@impresstv.com>
2015-04-02 17:22:32 +0200

    Changes for python2.6 compatibility

--- zkbrowser.py
+++ zkbrowser.py
@@ -43,7 +43,7 @@ logging.info('Kazoo connected')
 
 class RequestHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
-        logging.info('GET request: {}'.format(self.request.full_url()))
+        logging.info('GET request: {0}'.format(self.request.full_url()))
 
         zkPath = self.request.path
         zkDelete = self.get_query_argument('delete', default='no')
@@ -56,18 +56,18 @@ class RequestHandler(tornado.web.RequestHandler):
             url = url[:-1]
 
         if zkDelete == 'yes':
-            logging.debug('Deleting node: {}'.format(zkPath))
+            logging.debug('Deleting node: {0}'.format(zkPath))
 
             zk.delete(zkPath, recursive=True)
             self.redirect(url + '/..')
         elif zkSet == 'yes':
             zkSetValue = base64.b64decode(zkSetValueB64)
-            logging.debug('Setting node content: {} : {}'.format(zkPath, zkSetValue))
+            logging.debug('Setting node content: {0} : {1}'.format(zkPath, zkSetValue))
 
             zk.set(zkPath, zkSetValue)
             self.redirect(url)
         else:
-            logging.debug('Printing information of node {}'.format(zkPath))
+            logging.debug('Printing information of node {0}'.format(zkPath))
 
             zkChildren = sorted(zk.get_children(zkPath))
             raw_data = zk.get(zkPath)
